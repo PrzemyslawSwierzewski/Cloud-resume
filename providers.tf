9c26fe7183e496fbefa0fed388a992017cfdd8e9
PrzemyslawSwierzewski <110669323+PrzemyslawSwierzewski@users.noreply.github.com>
2026-03-04 13:03:01 +0100

    resource_provider_registrations changed to "none"

--- providers.tf
+++ providers.tf
@@ -12,5 +12,5 @@ provider "azurerm" {
 
   # Avoid deprecation warning about `skip_provider_registration` in v4.x
   # AzureRM v5 will require explicit registration blocks, so provide empty list.
-  resource_provider_registrations = []
+  resource_provider_registrations = "none"
 }
