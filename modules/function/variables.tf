0de9b43a7df3af5ebeb783a0a195d8256350c72c
PrzemyslawSwierzewski <110669323+PrzemyslawSwierzewski@users.noreply.github.com>
2026-03-04 14:27:46 +0100

    now

--- modules/function/variables.tf
+++ modules/function/variables.tf
@@ -3,4 +3,4 @@ variable "prefix" { type = string }
 variable "location" { type = string }
 variable "tags" { type = map(string) }
 variable "storage_account_for_function" { type = string }
-variable "cosmos_account_name" { type = string }
+#variable "cosmos_account_name" { type = string }
